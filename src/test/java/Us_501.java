4689112fc250a17e07e4b76a8cd209030177077d
efeersan <161042207+efeersan89@users.noreply.github.com>
2025-10-27 21:02:40 +0000

    efe branch opened

--- src/test/java/Us_501.java
+++ src/test/java/Us_501.java
@@ -2,4 +2,8 @@ public class Us_501 {
     public static void main(String[] args) {
         System.out.println("hadi yapma goreyim");
     }
+
+    public static void main(String[] args) {
+        System.out.println("deneme");
+    }
 }
