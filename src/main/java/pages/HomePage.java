64ecb841e50efa3f95074894e29241e8c7354ec4
tanerozc <tanerozcelik@gmail.com>
2025-10-28 12:22:14 +0000

    HomePage updated

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -2,7 +2,10 @@ package pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.bidi.log.Log;
 import org.openqa.selenium.support.FindBy;
+
+import javax.swing.*;
 
 public class HomePage extends BasePage {
 
@@ -10,8 +13,11 @@ public class HomePage extends BasePage {
         super(driver);
     }
 
+    @FindBy(xpath = "//div[@class='menu-toggle']")
+    private WebElement allMenu;
+
     @FindBy(xpath = "//ul[@class='top-menu mobile']//a[@href='/computers']")
-    private WebElement computers;
+    private WebElement computersButton;
 
     @FindBy(xpath = "//ul[@class='top-menu mobile']//a[@href='/electronics']")
     private WebElement electronicsButton;
@@ -45,4 +51,76 @@ public class HomePage extends BasePage {
 
     @FindBy (xpath = "//button[@type='submit']")
     private WebElement searchButton;
+
+
+    public void verifyTabMenu(){
+        verifyDisplayed(allMenu,"Categories");
+        LOGGER.info("All menu is displayed");
+    }
+
+    public void clickComputersButton(){
+        clickElement(computersButton);
+        LOGGER.info("Computers button clicked");
+    }
+
+    public void clickElectronicsButton(){
+        clickElement(electronicsButton);
+        LOGGER.info("Electronics button clicked");
+    }
+
+    public void clickApparelButton(){
+        clickElement(apparelButton);
+        LOGGER.info("Apparel button clicked");
+    }
+
+    public void clickDigitalDownloadsButton(){
+        clickElement(digitalDownloadsButton);
+        LOGGER.info("Digital downloads button clicked");
+    }
+
+    public void clickBooksButton(){
+        clickElement(booksButton);
+        LOGGER.info("Books button clicked");
+    }
+
+    public void clickJewelryButton(){
+        clickElement(jewelryButton);
+        LOGGER.info("Jewelry button clicked");
+    }
+
+    public void clickGiftCardsButton(){
+        clickElement(giftCardsButton);
+        LOGGER.info("Gift cards button clicked");
+    }
+
+    public void clickLoginButton(){
+        clickElement(loginButton);
+        LOGGER.info("Login button clicked");
+    }
+
+    public void clickRegisterButton(){
+        clickElement(registerButton);
+        LOGGER.info("Register button clicked");
+    }
+
+    public void clickShoppingCartButton(){
+        clickElement(shoppingCartButton);
+        LOGGER.info("Shopping cart clicked");
+    }
+
+    public void clickSearchText(){
+        clickElement(searchText);
+        LOGGER.info("Search text clicked");
+    }
+
+    public void clickSearchButton(){
+        clickElement(searchButton);
+        LOGGER.info("Search button clicked");
+    }
+
+
+
+
+
+
 }
