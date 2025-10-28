6f2b6032c812f99e46c282e404dbe5e8b4e4b9c0
tanerozc <tanerozcelik@gmail.com>
2025-10-28 21:18:16 +0000

    Login page created

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,4 +1,39 @@
 package pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage{
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy (xpath = "//input[@class='email']")
+    private WebElement emailInput;
+
+    @FindBy(xpath = "//input[@class='password']")
+    private WebElement passwordInput;
+
+    @FindBy(xpath = "//div[@class='buttons']//button[@type='submit']")
+    private WebElement loginButton;
+
+    @FindBy(xpath = "//div[@class='buttons']//button[@type='button']")
+    private WebElement registerButton;
+
+    public void fillLoginMask(final String email, final String password) {
+        sendKeysToElement(emailInput, email);
+        LOGGER.info("Email entered");
+        sendKeysToElement(passwordInput, password);
+        LOGGER.info("Password entered");
+    }
+
+    public void clickLoginBtn(){
+        clickElement(loginButton);
+        LOGGER.info("Login button clicked");
+    }
+
+
+
 }
